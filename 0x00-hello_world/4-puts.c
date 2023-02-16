679e224e381653d14657928c2787d019845c90de
Berna <abuyebarnabas@gmail.com>
2023-02-16 09:00:02 +0300

    first c program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*
+ * main - Entry point
+ *
+ * return - Always 0 (success)
+ */
+int main()
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	
+	return (0);
+}
