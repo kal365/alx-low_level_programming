9e9cf20754fe95167986041a073d9b2ff64e8b00
Berna <abuyebarnabas@gmail.com>
2023-02-16 17:14:05 +0300

    hotfix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (success)
+ * Return: Always 0
  */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	fprintf(stderr, "and that piece of art is useful\"-Dora Korpar,2015-10-19\n");
 	return (1);
 }
