e2cdc83a83db4abd911506c2770cb9e0ac684387
Berna <abuyebarnabas@gmail.com>
2023-02-16 09:21:39 +0300

    size in d/t arch

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("Size of a char: %i byte(s)\n");
+	printf("Size of an int: %i byte(s)\n");
+	printf("Size of a long int: %i byte(s)\n");
+	printf("Size of a long long int: %i byte(s)\n");
+	printf("Size of a float: 4 byte(s)\n");
+	return (0);
+}
