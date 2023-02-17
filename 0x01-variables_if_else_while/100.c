f4502bedd296e84e84de4e032bf33f0512766463
Berna <abuyebarnabas@gmail.com>
2023-02-17 19:09:05 +0300

    look up

--- 0x01-variables_if_else_while/100.c
+++ 0x01-variables_if_else_while/100.c
@@ -14,16 +14,9 @@ int main(void)
 		{
 			for (num3 = 2; num3 < 10; num3++)
 			{
-				if (num2 < num3)
-				{
-					putchar(num1 + '0');
-					putchar(num2 + '0');
-					putchar(num3 + '0');
-				}
-				else
-				{
-					continue;
-				}
+				putchar(num1 + '0');
+				putchar(num2 + '0');
+				putchar(num3 + '0');
 
 				if (num1 + num2 + num3 == 24)
 				{
