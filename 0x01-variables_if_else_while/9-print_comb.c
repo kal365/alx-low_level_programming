9cbbfd1d650c47ede81704835f253e86929f9ab9
Berna <abuyebarnabas@gmail.com>
2023-02-17 16:50:56 +0300

    print num

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int start;
+
+	for (start = 0; start < 10; start++)
+	{
+		putchar(start + '0');
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
