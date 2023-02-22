095149cf158b4b584e9b384cce06f52b30cfc7d5
Berna <abuyebarnabas@gmail.com>
2023-02-22 14:00:02 +0300

    Write a program that prints the first 98 Fibonacci numbers, starting with 1 and 2, followed by a new line.

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 on success
+ */
+int main(void)
+{
+	int start;
+
+	long int follow1, sum, follow2;
+
+	start = 0;
+
+	follow2 = 0;
+
+	follow1 = 1;
+
+	sum = 1;
+
+	while (start <= 98)
+	{
+		if (start < 3)
+		{
+			if (start == 2)
+			{
+				printf("%i, ", start);
+			}
+			sum = follow1 + follow2;
+			follow2 = follow1;
+			follow1 = sum;
+			start++;
+		}
+		if (start == 50)
+			printf("%lu", sum);
+		else
+			printf("%lu, ", sum);
+		sum = follow1 + follow2;
+		follow2 = follow1;
+		follow1 = sum;
+		start++;
+	}
+	printf("\n");
+	return (0);
+}
