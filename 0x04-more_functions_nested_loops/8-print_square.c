4d2593e7562fd0fd74a2e9e83336c56b19bbda13
Berna <abuyebarnabas@gmail.com>
2023-02-23 18:45:26 +0300

    Write a function that prints a square, followed by a new line

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_square - to print square with hash
+ * Return: 0 on success
+ */
+void print_square(int size)
+{
+	int start, hash;
+
+	start = 0;
+
+	while (start < size)
+	{
+		hash = 0;
+		while (hash < size)
+		{
+			_putchar('#');
+			hash++;
+		}
+		_putchar('\n');
+		start++;
+	}
+}
