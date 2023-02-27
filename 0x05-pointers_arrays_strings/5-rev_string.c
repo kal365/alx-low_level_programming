af4327d5aaec294221133b71bee05061e817c118
Berna <abuyebarnabas@gmail.com>
2023-02-27 20:22:27 +0300

    mirror

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_rev - To reverse input string
+ * @s: pointer variable
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int i, j;
+
+	char str[500];
+
+	i = 0;
+
+	while (*(s + i))
+	{
+		*(str + i) = *(s + i);
+		i++;
+	}
+	i = i - 1;
+	j = 0;
+	while (i >= 0)
+	{
+		*(s + i) = *(str + j);
+		j++;
+		i--;
+	}
+}
