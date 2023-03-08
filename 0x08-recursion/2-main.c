d5b7e09123af95b69d5c4d3ee6ffbfb057f671d4
Berna <abuyebarnabas@gmail.com>
2023-03-08 22:07:21 +0300

    fixe

--- 0x08-recursion/2-main.c
+++ 0x08-recursion/2-main.c
@@ -10,7 +10,7 @@ int main(void)
 {
     int n;
 
-    n = _strlen_recursion("Corbin Coleman");
+    n = _strlen_recursion("You mustn't be afraid to dream a little bigger, darling.");
     printf("%d\n", n);
     return (0);
 }
